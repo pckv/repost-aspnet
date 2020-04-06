e718a63d7492e1a10fbe3dc21d1bfe5fa1ec2923
Per Christian Kvalvik <pc@kvalvik.no>
2020-04-06 18:30:30 +0200

    Add relation to owner and update edit model to match

--- Models/Resub.cs
+++ Models/Resub.cs
@@ -1,26 +1,48 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace RepostAspNet.Models
 {
+    [Table("resubs")]
     public class Resub
     {
-        [JsonIgnore]
-        public int Id { get; set; }
+        [JsonIgnore] public int Id { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime Created { get; set; }
-        public DateTime Edited { get; set; }
+        public DateTime? Edited { get; set; }
+
+        [JsonIgnore] public User Owner { get; set; }
+        [JsonPropertyName("owner_username")] public string OwnerUsername => Owner.Username;
+
+        public bool IsOwner(User user)
+        {
+            return user == Owner;
+        }
+
+        public bool IsAllowedToEdit(User user)
+        {
+            return IsOwner(user);
+        }
     }
-    
+
     public class CreateResub
     {
         public string Name { get; set; }
         public string Description { get; set; }
     }
 
-    public class EditResub
+    public class EditResub : EditModel
     {
-        public string Description { get; set; }
+        public string Description
+        {
+            get => (string) GetField(nameof(Description));
+            set => SetField(nameof(Description), value);
+        }
+
+        [JsonPropertyName("new_owner_username")]
+        public string NewOwnerUsername { get; set; }
     }
 }
