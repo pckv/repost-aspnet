372c039e72435a19f41c24ffd8bcaaf5dc1797b0
Per Christian Kvalvik <pc@kvalvik.no>
2020-04-20 22:09:05 +0200

    Remove client_secret and environment variable configuration
    Environment variable configuration applies to client_id and JWT signing key. Configuration has two variations: json-like PascalCase syntax and environment variable UPPERCASE_SNAKE_CASE syntax.
    
    IdentityServer4 does not support HS256, and therefore a certificate must be used.

--- Config.cs
+++ Config.cs
@@ -1,25 +1,74 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using IdentityServer4.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace RepostAspNet
 {
-    public class Config
+    public interface IConfig
     {
-        public static IEnumerable<ApiResource> Apis => new[]
+        IEnumerable<ApiResource> Apis { get; }
+        IEnumerable<Client> Clients { get; }
+        X509Certificate2 SigningCredential { get; }
+    }
+
+    public class Config : IConfig
+    {
+        private readonly List<(LogLevel, string)> _log;
+
+        public Config(IConfiguration configuration, List<(LogLevel, string)> log)
+        {
+            Configuration = configuration;
+            _log = log;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IEnumerable<ApiResource> Apis => new[]
         {
             // The name of the resource will also be the name of the scope
             new ApiResource("user", "User")
         };
 
-        public static IEnumerable<Client> Clients => new[]
+        public IEnumerable<Client> Clients => new[]
         {
             new Client
             {
-                ClientId = "client",
-                ClientSecrets = {new Secret("secret".Sha256())},
+                ClientId = Configuration["ClientId"] ?? Configuration["CLIENT_ID"] ?? "repost",
+                RequireClientSecret = false,
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                 AllowedScopes = {"user"}
             }
         };
+
+        public X509Certificate2 SigningCredential
+        {
+            get
+            {
+                var path = Configuration["SigningCredential.Path"] ?? Configuration["SIGNING_CREDENTIAL_PATH"];
+                if (string.IsNullOrWhiteSpace(path))
+                {
+                    return null;
+                }
+
+                if (!File.Exists(path))
+                {
+                    _log.Add((LogLevel.Error, "Provided signing credential does not exist"));
+                    return null;
+                }
+
+                var password = Configuration["SigningCredential.Password"] ??
+                               Configuration["SIGNING_CREDENTIAL_PASSWORD"];
+                if (string.IsNullOrWhiteSpace(password))
+                {
+                    _log.Add((LogLevel.Warning, "Signing credential was provided without password"));
+                    return null;
+                }
+
+                return new X509Certificate2(path, password);
+            }
+        }
     }
 }
